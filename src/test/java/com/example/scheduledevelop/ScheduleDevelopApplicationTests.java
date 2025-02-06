34879a1bad7d0b10792865ec842e9fc51dd7c833
yoorkim <amy0848@naver.com>
2025-02-06 18:38:00 +0900

    init

---
+++ src/test/java/com/example/scheduledevelop/ScheduleDevelopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.scheduledevelop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleDevelopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
